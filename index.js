9ab7321084578c73fb6ed9c7f35e28648620f157
princegupta-in <zprincegupta@gmail.com>
2024-12-06 21:07:44 +0530

    morgan and winston tutorial

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 import 'dotenv/config'
 import express from 'express'
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express()
 const port = process.env.PORT || 3000
-
+const morganFormat = ":method :url :status :response-time ms";
+app.use(
+    morgan(morganFormat, {
+        stream: {
+            write: (message) => {
+                const logObject = {
+                    method: message.split(" ")[0],
+                    url: message.split(" ")[1],
+                    status: message.split(" ")[2],
+                    responseTime: message.split(" ")[3],
+                };
+                logger.info(JSON.stringify(logObject));
+            },
+        },
+    })
+);
 //+++++++++++++TO SEND THE DATA++++++++++++++++++
 
 
@@ -52,24 +69,24 @@ app.get("/teas/:dhaiChudaID", (req, res) => {
 
 })
 //update tea
-app.put('/teas/:id',(req,res)=>{
+app.put('/teas/:id', (req, res) => {
     const tea = teaData.find(t => t.id === parseInt(req.params.id))
-    if(!tea){
+    if (!tea) {
         return res.status(404).send("Tea Not Found")
     }
-    const {name, price} = req.body
+    const { name, price } = req.body
     tea.name = name;
     tea.price = price;
     res.status(200).send(tea)
 })
 //delete tea
-app.delete('/teas/:id',(req,res)=>{
+app.delete('/teas/:id', (req, res) => {
     // const tea = teaData.find(t => t.id === parseInt(req.params.id))
     const index = teaData.findIndex(t => t.id === parseInt(req.params.id))
-    if(index === -1){
+    if (index === -1) {
         return res.status(404).send("Tea Already Not exist")
     }
-    teaData.splice(index,index)
+    teaData.splice(index, index)
     return res.status(200).send("Deleted")
 
 })
